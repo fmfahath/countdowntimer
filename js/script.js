06e0f13b0cc708b12baa25e7619c620ee21e851b
fmfahath <fmfahath.dev@gmail.com>
2023-01-12 13:09:56 +0530

    js : het days/hrs/min/sec

--- js/script.js
+++ js/script.js
@@ -3,12 +3,24 @@
     const currentYear = new Date().getFullYear();
     //set new year
     const newYear = new Date(`January 1 ${currentYear+1} 00:00:00`);
-    //different days count between new year current & 
+    //different days count between new year & current year
     const diff = newYear - currentYear;
     
+    //-------------------------------------------
+    // 1000ms = 1s
+    // 60s = 1m
+    // 60m = 1h
+    // 24hrs = 1day
+    //--------------------------------------------
+
+    const d = Math.floor(diff/1000/60/60/24);  //find days
+    const h = Math.floor((diff/1000/60/60)%24); //find hours
+    const m = Math.floor((diff/1000/69)%60); //find minutes
+    const s = Math.floor((diff/1000)%60); //find seconds
+
+    // console.log(d + ":" + h + ":" + m + ":" + s );
+
     
-    // console.log(newYear)
 
+ 
  }
-
- updateTime();
