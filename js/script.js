74120ac2f9286a785ee7536fd3ea54321dba4001
fmfahath <fmfahath.dev@gmail.com>
2023-01-12 13:23:24 +0530

    js : set DOM to display count / set interval func

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
  function updateTime(){
+"#"  
+    const days  = document.querySelector("#days");
+    const hours = document.querySelector("#hours");
+    const minutes = document.querySelector("#minutes");
+    const seconds = document.querySelector("#seconds");
+"#"  
     //get current year
     const currentYear = new Date().getFullYear();
     //set new year
@@ -17,10 +23,13 @@
     const h = Math.floor((diff/1000/60/60)%24); //find hours
     const m = Math.floor((diff/1000/69)%60); //find minutes
     const s = Math.floor((diff/1000)%60); //find seconds
-
-    // console.log(d + ":" + h + ":" + m + ":" + s );
-
     
+    //display on screen   
+    days.innerHTML = d;   
+    hours.innerHTML = h; 
+    minutes.innerHTML = m;   
+    seconds.innerHTML = s;   
 
- 
  }
+
+setInterval(updateTime, 1000);
